07e60fc870381db148816fa0568c4bb4c64f6749
StringImad <73947304+StringImad@users.noreply.github.com>
2023-04-09 22:10:25 +0200

    subida desde casa

--- Parte1/Practica2/index.php
+++ Parte1/Practica2/index.php
@@ -18,9 +18,40 @@ if (isset($_POST['btnEntrar'])) {
 
     if (!$error_form) {
         //conexion
+        try {
+            $conexion = mysqli_connect(SERVIDOR_BD, USUARIO_BD, CLAVE_BD, NOMBRE_BD);
+            mysqli_set_charset($conexion, "utf8");
+        } catch (Exception $e) {
+            $mensaje = "Imposible conectar con la BD. Error Nº " . mysqli_connect_errno() . " : " . mysqli_connect_error();
+            session_destroy();
+            die(error_page("Primer Login", "Primer Login", $mensaje));
+        }
+
+        //consulata con la BD
+        try {
+            $consulta = "select * from usuarios where usuario='" . $_POST["usuario"] . "' and clave='" . md5($_POST["clave"]) . "'";
+            $resultado = mysqli_query($conexion, $consulta);
+            $usuario_registrado = mysqli_num_rows($resultado) > 0;
+
+            mysqli_free_result($resultado);
+            mysqli_close($conexion);
+            if ($usuario_registrado) {
+                $_SESSION["usuario"] = $_POST["usuario"];
+                $_SESSION["clave"] = md5($_POST["clave"]);
+                $_SESSION["ultimo_acceso"] = time();
+                header("Location:index.php");
+                exit;
+            } else {
+                $error_usuario = true;
+            }
+        } catch (Exception $e) {
+            $mensaje = "Imposible realizar la consulta. Error Nº " . mysqli_errno($conexion) . " : " . mysqli_error($conexion);
+            mysqli_close($conexion);
+            session_destroy();
+            die(error_page("Primer Login", "Primer Login", $mensaje));
+        }
     }
 }
-
 //boton enviar el formulario del registro
 if (isset($_POST['btnEnviar'])) {
     $error_usuario = $_POST['usuario'] == "";
@@ -43,6 +74,25 @@ if (isset($_POST['btnEnviar'])) {
         } catch (Exception $e) {
             die("Imposible conectar. Error Nº " . mysqli_connect_errno() . " : " . mysqli_connect_error());
         }
+
+        $consulta = "insert into usuarios(nombre,usuario,clave,dni,sexo,foto,suscrip,tipo) values ('" . $_POST["nombre"] . "','" . $_POST["usuario"] . "','" . md5($_POST["clave"]) . "','" . $_POST["dni"] . "','" . $_POST["sexo"] . "','" . $_FILES["foto"]["name"] . "','" . $_POST["suscrip"] . "','admin')";
+
+        try {
+
+            mysqli_query($conexion, $consulta);
+
+            $_SESSION["usuario"] = $_POST["usuario"];
+            $_SESSION["clave"] = $_POST["clave"];
+
+            mysqli_close($conexion);
+            header("Location:index.php");
+            exit();
+        } catch (Exception $e) {
+
+            $mensaje = "Imposible realizar la consulta. Error Nº " . mysqli_errno($conexion) . ": " . mysqli_error($conexion);
+            mysqli_close($conexion);
+            die(error_page("Prácitca 1º CRUD", "Nuevo Usuario", $mensaje));
+        }
     }
 }
 
@@ -63,14 +113,45 @@ if (isset($_POST['btnEnviar'])) {
 
     <?php
 
-    if ((isset($_POST["btnEntrar"]) && !$error_form) || (isset($_POST["btnEnviar"]) && !$error_formulario_registro)) {
-        require "vistas/vista_info.php";
-    } else if (isset($_POST["btnRegistro"])) {
-        require "vistas/vista_formulario_registro.php";
-    } else if (isset($_POST["btnEnviar"]) && $error_formulario_registro) {
-        require "vistas/vista_formulario_registro.php";
 
+    // //si le da al boton de registro se reenvia al formulario de registro
+    // if (isset($_POST["btnRegistro"])) {
+    //     require "vistas/vista_formulario_registro.php";
+    // }
+    // //Si ha enviado el formulario y hay error en el formulario de registro se vuelve a mostrar el formulario
+    // if (isset($_POST["btnEnviar"]) && $error_formulario_registro) {
+    //     require "vistas/vista_formulario_registro.php";
+    
+    // }
+    
+    // if ((isset($_POST["btnEntrar"]) && !$error_form) || (isset($_POST["btnEnviar"]) && !$error_formulario_registro)) {
+    //     require "vistas/vista_info.php";
+    // } else {
+    //     require "vistas/vista_formulario_login.php";
+    
+    // }
+    define("MINUTOS", 10);
+
+    session_name("primer_login_22_33");
+    session_start();
+    //Estas 3 variables existen cuando me he logeado, si no, no cumples ninguna
+    if (isset($_SESSION["usuario"]) && isset($_SESSION["clave"]) && (isset($_SESSION["ultimo_acceso"]))) {
+
+        require "src/seguridad.php";
+
+        $_SESSION["ultimo_acceso"] = time();
+
+        if ($datos_usuario_log["tipo"] == "normal")
+            require "vistas/vista_normal.php";
+        else
+            require "vistas/vista_admin.php";
+
+        mysqli_close($conexion);
+    } elseif (isset($_POST["btnRegistro"]) || isset($_POST["btnEnviar"])) {
+
+        require "vistas/vista_formulario_registro.php";
     } else {
+
         require "vistas/vista_formulario_login.php";
     }
 
--- Parte1/Practica2/src/funciones.php
+++ Parte1/Practica2/src/funciones.php
@@ -14,5 +14,30 @@ function dni_valido($dni)
     return LetraNIF(substr($dni, 0, 8)) == strtoupper(substr($dni, -1));
 }
 
+function repetido($conexion, $tabla,$columna,$valor,$columna_clave=null,$valor_clave=null)
+{
+    if(isset($columna_clave))
+        $consulta="select ".$columna." from ".$tabla." where ".$columna."='".$valor."' AND ".$columna_clave."<>'".$valor_clave."'";
+    else
+        $consulta="select ".$columna." from ".$tabla." where ".$columna."='".$valor."'";
+    try
+    {
+        $resultado=mysqli_query($conexion,$consulta);
+        $respuesta=mysqli_num_rows($resultado)>0;
+        mysqli_free_result($resultado);
+    }
+    catch(Exception $e)
+    {
+        $respuesta ="Imposible realizar la consulta. Error Nº ".mysqli_errno($conexion)." : ".mysqli_error($conexion); 
+    }
+        
+    return $respuesta;
+}
+
+function error_page($title,$encabezado,$mensaje)
+{
+    return "<!DOCTYPE html><html lang='es'><head><meta charset='UTF-8'/><title>".$title."</title></head><body><h1>".$encabezado."</h1><p>".$mensaje."</p></body></html>";
+    
+}
 
 ?>
