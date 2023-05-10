415a51b845d640faaad9264539c4db80da47ad50
imad <imadelfahssi@gmail.com>
2023-05-10 11:56:47 +0200

    subida desde clase

--- Parte2/Pract3_SW/Servicios_Webs/servicios_rest_login/index.php
+++ Parte2/Pract3_SW/Servicios_Webs/servicios_rest_login/index.php
@@ -3,70 +3,124 @@ require "src/funciones.php";
 require __DIR__ . '/Slim/autoload.php';
 
 
-$app= new \Slim\App;
+$app = new \Slim\App;
 
 
 
 
+$app->post('/salir', function ($request) {
+    session_id($request->getParam("api_session"));
 
-$app->post('/login',function($request){
-    
-    $datos[]=$request->getParam("usuario");
-    $datos[]=$request->getParam("clave");
+    session_start();
+
+    session_destroy();
+    echo json_encode(array('logout' => 'Close session'));
+});
+
+$app->post('/logueado', function ($request) {
+    session_id($request->getParam("api_session"));
+
+    session_start();
+
+    if(isset($_SESSION["tipo"])){
+        $datos[]=$_SESSION["tipo"];
+        $datos[]=$_SESSION["clave"];
+        echo json_encode(logueado($datos));
+    }else{
+        session_destroy();
+        echo json_encode(array('no_login'=>'No logueado'));
+    }
+});
+
+$app->post('/login', function ($request) {
+
+    $datos[] = $request->getParam("usuario");
+    $datos[] = $request->getParam("clave");
 
 
     echo json_encode(login($datos));
-
 });
 
-$app->get('/obtener_usuarios',function(){
+$app->get('/obtener_usuarios', function ($request) {
+    session_id($request->getParam("api_session"));
 
-    echo json_encode(obtener_usuarios());
+    session_start();
+    if(isset($_SESSION["tipo"]) && $_SESSION["tipo"]=="admin") {
+        echo json_encode(obtener_usuarios());
+
+    }else{
+        session_destroy();
+        echo json_encode(array('no_login'=>'No logueado'));
+    }
 });
 
-$app->get('/obtener_usuario/{id}',function($request){
+$app->get('/obtener_usuario/{id}', function ($request) {
+    if(isset($_SESSION["tipo"]) && $_SESSION["tipo"]=="admin") {
+        echo json_encode(obtener_usuario($request->getAttribute('id')));
 
-    echo json_encode(obtener_usuario($request->getAttribute('id')));
+    }else{
+        session_destroy();
+        echo json_encode(array('no_login'=>'No logueado'));
+    }
 });
 
-$app->delete('/borrar_usuario/{id}',function($request){
+$app->delete('/borrar_usuario/{id}', function ($request) {
 
     echo json_encode(borrar_usuario($request->getAttribute('id')));
 });
 
 
-$app->get('/repetido_reg/{columna}/{valor}',function($request){
+$app->get('/repetido_reg/{columna}/{valor}', function ($request) {
 
-    echo json_encode(repetido($request->getAttribute('columna'),$request->getAttribute('valor')));
+    if(isset($_SESSION["tipo"]) && $_SESSION["tipo"]=="admin") {
+        echo json_encode(repetido($request->getAttribute('columna'), $request->getAttribute('valor')));
+
+    }else{
+        session_destroy();
+        echo json_encode(array('no_login'=>'No logueado'));
+    }
+
 });
 
-$app->get('/repetido_edit/{columna}/{valor}/{columna_id}/{valor_id}',function($request){
+$app->get('/repetido_edit/{columna}/{valor}/{columna_id}/{valor_id}', function ($request) {
 
-    echo json_encode(repetido($request->getAttribute('columna'),$request->getAttribute('valor'),$request->getAttribute('columna_id'),$request->getAttribute('valor_id')));
+    echo json_encode(repetido($request->getAttribute('columna'), $request->getAttribute('valor'), $request->getAttribute('columna_id'), $request->getAttribute('valor_id')));
 });
 
-$app->post('/insertar_usuario',function($request){
+$app->post('/insertar_usuario', function ($request) {
 
-    $datos[]=$request->getParam("usuario");
-    $datos[]=$request->getParam("clave");
-    $datos[]=$request->getParam("nombre");
-    $datos[]=$request->getParam("dni");
-    $datos[]=$request->getParam("sexo");
-    $datos[]=$request->getParam("subs");
+    if(isset($_SESSION["tipo"]) && $_SESSION["tipo"]=="admin") {
 
-    echo json_encode(insertar_usuario($datos));
+        $datos[] = $request->getParam("usuario");
+        $datos[] = $request->getParam("clave");
+        $datos[] = $request->getParam("nombre");
+        $datos[] = $request->getParam("dni");
+        $datos[] = $request->getParam("sexo");
+        $datos[] = $request->getParam("subs");
+    
+        echo json_encode(insertar_usuario($datos));
+    }else{
+        session_destroy();
+        echo json_encode(array('no_login'=>'No logueado'));
+    }
+
+
 });
 
-$app->put('/modificar_usuario/{id_usuario}',function($request){
+$app->put('/modificar_usuario/{id_usuario}', function ($request) {
 
-    echo json_encode(modificar_usuario($request->getAttribute("id_usuario"),$request->getParam("usuario"),$request->getParam("clave"),$request->getParam("nombre"),$request->getParam("dni"),$request->getParam("sexo"),$request->getParam("subs")));
+    echo json_encode(modificar_usuario($request->getAttribute("id_usuario"), $request->getParam("usuario"), $request->getParam("clave"), $request->getParam("nombre"), $request->getParam("dni"), $request->getParam("sexo"), $request->getParam("subs")));
 });
 
-$app->put('/cambiar_foto/{id}',function($request){
+$app->put('/cambiar_foto/{id}', function ($request) {
+    if(isset($_SESSION["tipo"]) && $_SESSION["tipo"]=="admin") {
+        echo json_encode(cambiar_foto($request->getAttribute('id'), $request->getParam('foto')));
 
-    echo json_encode(cambiar_foto($request->getAttribute('id'),$request->getParam('foto')));
+    }else{
+        session_destroy();
+        echo json_encode(array('no_login'=>'No logueado'));
+    }
 });
 
 // Una vez creado servicios los pongo a disposición
 $app->run();
-?>
--- Parte2/Pract3_SW/Servicios_Webs/servicios_rest_login/src/funciones.php
+++ Parte2/Pract3_SW/Servicios_Webs/servicios_rest_login/src/funciones.php
@@ -3,33 +3,59 @@ require "bd_config.php";
 
 function login($datos)
 {
-    try
-    {
-        $conexion=new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")); 
-        try
-        {
-            $consulta="select * from usuarios where usuario=? and clave=?";
-            $sentencia=$conexion->prepare($consulta);
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        try {
+            $consulta = "select * from usuarios where usuario=? and clave=?";
+            $sentencia = $conexion->prepare($consulta);
             $sentencia->execute($datos);
 
-            if($sentencia->rowCount()>0)
-                $respuesta["usuario"]=$sentencia->fetch(PDO::FETCH_ASSOC);
-            else
-                $respuesta["mensaje"]="Usuario no registrado en BD";
+            if ($sentencia->rowCount() > 0) {
+                $respuesta["usuario"] = $sentencia->fetch(PDO::FETCH_ASSOC);
+                session_name("api_login_seg_22_23");
+                session_start();
+                $_SESSION["usuario"] = $respuesta["usuario"]["usuario"];
 
-            $sentencia=null;
-            $conexion=null;
-        }
-        catch(PDOException $e)
-        {
-            $respuesta["mensaje_error"]="Imposible realizar la consulta. Error:".$e->getMessage();
-        }
-        
+                $_SESSION["clave"] = $respuesta["usuario"]["clave"];
+                $_SESSION["tipo"] = $respuesta["usuario"]["tipo"];
+                $respuesta["api_session"] = session_id();
+            } else
+                $respuesta["mensaje"] = "Usuario no registrado en BD";
 
+            $sentencia = null;
+            $conexion = null;
+        } catch (PDOException $e) {
+            $respuesta["mensaje_error"] = "Imposible realizar la consulta. Error:" . $e->getMessage();
+        }
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "Imposible conectar a la BD. Error:" . $e->getMessage();
     }
-    catch(PDOException $e)
-    {
-        $respuesta["mensaje_error"]="Imposible conectar a la BD. Error:".$e->getMessage();
+
+    return $respuesta;
+}
+
+function logueado($datos)
+{
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        try {
+            $consulta = "select * from usuarios where usuario=? and clave=?";
+            $sentencia = $conexion->prepare($consulta);
+            $sentencia->execute($datos);
+
+            if ($sentencia->rowCount() > 0) {
+                $respuesta["usuario"] = $sentencia->fetch(PDO::FETCH_ASSOC);
+        
+            } else
+                $respuesta["mensaje"] = "Usuario no registrado en BD";
+
+            $sentencia = null;
+            $conexion = null;
+        } catch (PDOException $e) {
+            $respuesta["mensaje_error"] = "Imposible realizar la consulta. Error:" . $e->getMessage();
+        }
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "Imposible conectar a la BD. Error:" . $e->getMessage();
     }
 
     return $respuesta;
@@ -37,31 +63,23 @@ function login($datos)
 
 function obtener_usuarios()
 {
-    try
-    {
-        $conexion=new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")); 
-        try
-        {
-            $consulta="select * from usuarios";
-            $sentencia=$conexion->prepare($consulta);
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        try {
+            $consulta = "select * from usuarios";
+            $sentencia = $conexion->prepare($consulta);
             $sentencia->execute();
 
-            $respuesta["usuarios"]=$sentencia->fetchAll(PDO::FETCH_ASSOC);
-            
+            $respuesta["usuarios"] = $sentencia->fetchAll(PDO::FETCH_ASSOC);
 
-            $sentencia=null;
-            $conexion=null;
-        }
-        catch(PDOException $e)
-        {
-            $respuesta["mensaje_error"]="Imposible realizar la consulta. Error:".$e->getMessage();
-        }
-        
 
-    }
-    catch(PDOException $e)
-    {
-        $respuesta["mensaje_error"]="Imposible conectar a la BD. Error:".$e->getMessage();
+            $sentencia = null;
+            $conexion = null;
+        } catch (PDOException $e) {
+            $respuesta["mensaje_error"] = "Imposible realizar la consulta. Error:" . $e->getMessage();
+        }
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "Imposible conectar a la BD. Error:" . $e->getMessage();
     }
 
     return $respuesta;
@@ -69,33 +87,25 @@ function obtener_usuarios()
 
 function obtener_usuario($id)
 {
-    try
-    {
-        $conexion=new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")); 
-        try
-        {
-            $consulta="select * from usuarios where id_usuario=?";
-            $sentencia=$conexion->prepare($consulta);
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        try {
+            $consulta = "select * from usuarios where id_usuario=?";
+            $sentencia = $conexion->prepare($consulta);
             $sentencia->execute([$id]);
 
-            if($sentencia->rowCount()>0)
-                $respuesta["usuario"]=$sentencia->fetch(PDO::FETCH_ASSOC);
+            if ($sentencia->rowCount() > 0)
+                $respuesta["usuario"] = $sentencia->fetch(PDO::FETCH_ASSOC);
             else
-                $respuesta["mensaje"]="Usuario no registrado en BD";
+                $respuesta["mensaje"] = "Usuario no registrado en BD";
 
-            $sentencia=null;
-            $conexion=null;
+            $sentencia = null;
+            $conexion = null;
+        } catch (PDOException $e) {
+            $respuesta["mensaje_error"] = "Imposible realizar la consulta. Error:" . $e->getMessage();
         }
-        catch(PDOException $e)
-        {
-            $respuesta["mensaje_error"]="Imposible realizar la consulta. Error:".$e->getMessage();
-        }
-        
-
-    }
-    catch(PDOException $e)
-    {
-        $respuesta["mensaje_error"]="Imposible conectar a la BD. Error:".$e->getMessage();
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "Imposible conectar a la BD. Error:" . $e->getMessage();
     }
 
     return $respuesta;
@@ -103,134 +113,104 @@ function obtener_usuario($id)
 
 function borrar_usuario($id)
 {
-    try
-    {
-        $conexion=new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")); 
-        try
-        {
-            $consulta="delete from usuarios where id_usuario=?";
-            $sentencia=$conexion->prepare($consulta);
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        try {
+            $consulta = "delete from usuarios where id_usuario=?";
+            $sentencia = $conexion->prepare($consulta);
             $sentencia->execute([$id]);
 
-            $respuesta["mensaje"]="Usuario borrado de la BD";
+            $respuesta["mensaje"] = "Usuario borrado de la BD";
 
-            $sentencia=null;
-            $conexion=null;
+            $sentencia = null;
+            $conexion = null;
+        } catch (PDOException $e) {
+            $respuesta["mensaje_error"] = "Imposible realizar la consulta. Error:" . $e->getMessage();
         }
-        catch(PDOException $e)
-        {
-            $respuesta["mensaje_error"]="Imposible realizar la consulta. Error:".$e->getMessage();
-        }
-        
-
-    }
-    catch(PDOException $e)
-    {
-        $respuesta["mensaje_error"]="Imposible conectar a la BD. Error:".$e->getMessage();
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "Imposible conectar a la BD. Error:" . $e->getMessage();
     }
 
     return $respuesta;
 }
 
 
-function repetido($columna,$valor,$columna_id=null,$valor_id=null)
+function repetido($columna, $valor, $columna_id = null, $valor_id = null)
 {
-    try
-    {
-        $conexion=new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")); 
-        try
-        {
-            if(isset($columna_id))
-            {
-                $consulta="select ".$columna." from usuarios where ".$columna."=? AND ".$columna_id."<>?";
-                $datos_consulta[]=$valor;
-                $datos_consulta[]=$valor_id;
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        try {
+            if (isset($columna_id)) {
+                $consulta = "select " . $columna . " from usuarios where " . $columna . "=? AND " . $columna_id . "<>?";
+                $datos_consulta[] = $valor;
+                $datos_consulta[] = $valor_id;
+            } else {
+                $consulta = "select " . $columna . " from usuarios where " . $columna . "=?";
+                $datos_consulta[] = $valor;
             }
-            else
-            {
-                $consulta="select ".$columna." from usuarios where ".$columna."=?";
-                $datos_consulta[]=$valor;
-            }
-            
-            
-            $sentencia=$conexion->prepare($consulta);
+
+
+            $sentencia = $conexion->prepare($consulta);
             $sentencia->execute($datos_consulta);
 
-            $respuesta["repetido"]=$sentencia->rowCount()>0;
-            
+            $respuesta["repetido"] = $sentencia->rowCount() > 0;
 
-            $sentencia=null;
-            $conexion=null;
-        }
-        catch(PDOException $e)
-        {
-            $respuesta["mensaje_error"]="Imposible realizar la consulta. Error:".$e->getMessage();
-        }
-        
 
-    }
-    catch(PDOException $e)
-    {
-        $respuesta["mensaje_error"]="Imposible conectar a la BD. Error:".$e->getMessage();
+            $sentencia = null;
+            $conexion = null;
+        } catch (PDOException $e) {
+            $respuesta["mensaje_error"] = "Imposible realizar la consulta. Error:" . $e->getMessage();
+        }
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "Imposible conectar a la BD. Error:" . $e->getMessage();
     }
 
     return $respuesta;
 }
 
-function modificar_usuario($id_usuario,$usuario,$clave,$nombre,$dni,$sexo,$subs)
+function modificar_usuario($id_usuario, $usuario, $clave, $nombre, $dni, $sexo, $subs)
 {
-    try
-    {
-        $conexion=new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")); 
-        try
-        {
-            
-            if($clave=="")
-            {
-                $consulta="update usuarios set usuario=?, nombre=?, dni=?, sexo=?, subscripcion=? where id_usuario=?";
-                $datos[]=$usuario;
-                $datos[]=$nombre;
-                $datos[]=$dni;
-                $datos[]=$sexo;
-                $datos[]=$subs;
-                $datos[]=$id_usuario;
-            }
-            else
-            {
-                $consulta="update usuarios set usuario=?, nombre=?, clave=?, dni=?, sexo=?, subscripcion=? where id_usuario=?";
-                $datos[]=$usuario;
-                $datos[]=$nombre;
-                $datos[]=$clave;
-                $datos[]=$dni;
-                $datos[]=$sexo;
-                $datos[]=$subs;
-                $datos[]=$id_usuario;
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        try {
+
+            if ($clave == "") {
+                $consulta = "update usuarios set usuario=?, nombre=?, dni=?, sexo=?, subscripcion=? where id_usuario=?";
+                $datos[] = $usuario;
+                $datos[] = $nombre;
+                $datos[] = $dni;
+                $datos[] = $sexo;
+                $datos[] = $subs;
+                $datos[] = $id_usuario;
+            } else {
+                $consulta = "update usuarios set usuario=?, nombre=?, clave=?, dni=?, sexo=?, subscripcion=? where id_usuario=?";
+                $datos[] = $usuario;
+                $datos[] = $nombre;
+                $datos[] = $clave;
+                $datos[] = $dni;
+                $datos[] = $sexo;
+                $datos[] = $subs;
+                $datos[] = $id_usuario;
             }
 
-            
-            $sentencia=$conexion->prepare($consulta);
+
+            $sentencia = $conexion->prepare($consulta);
             $sentencia->execute($datos);
 
-            $respuesta["mensaje"]="Se ha modificado correctamente el usuario";
-            
+            $respuesta["mensaje"] = "Se ha modificado correctamente el usuario";
 
-            $sentencia=null;
-            $conexion=null;
-        }
-        catch(PDOException $e)
-        {
-            
-            $respuesta["mensaje_error"]="Imposible realizar la consulta. Error:".$e->getMessage();
-        }
 
-    }
-    catch(PDOException $e)
-    {
-        $respuesta["mensaje_error"]="Imposible conectar a la BD. Error:".$e->getMessage();
+            $sentencia = null;
+            $conexion = null;
+        } catch (PDOException $e) {
+
+            $respuesta["mensaje_error"] = "Imposible realizar la consulta. Error:" . $e->getMessage();
+        }
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "Imposible conectar a la BD. Error:" . $e->getMessage();
     }
 
     return $respuesta;
-
 }
 
 
@@ -238,68 +218,51 @@ function modificar_usuario($id_usuario,$usuario,$clave,$nombre,$dni,$sexo,$subs)
 
 function insertar_usuario($datos)
 {
-    try
-    {
-        $conexion=new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")); 
-        try
-        {
-            $consulta="insert into usuarios(usuario, clave, nombre, dni,sexo, subscripcion) values(?,?,?,?,?,?)";
-            
-            $sentencia=$conexion->prepare($consulta);
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        try {
+            $consulta = "insert into usuarios(usuario, clave, nombre, dni,sexo, subscripcion) values(?,?,?,?,?,?)";
+
+            $sentencia = $conexion->prepare($consulta);
             $sentencia->execute($datos);
 
-            $respuesta["ultimo_id"]=$conexion->lastInsertId();
-            
+            $respuesta["ultimo_id"] = $conexion->lastInsertId();
 
-            $sentencia=null;
-            $conexion=null;
-        }
-        catch(PDOException $e)
-        {
-            $respuesta["mensaje_error"]="Imposible realizar la consulta. Error:".$e->getMessage();
-        }
-        
 
-    }
-    catch(PDOException $e)
-    {
-        $respuesta["mensaje_error"]="Imposible conectar a la BD. Error:".$e->getMessage();
+            $sentencia = null;
+            $conexion = null;
+        } catch (PDOException $e) {
+            $respuesta["mensaje_error"] = "Imposible realizar la consulta. Error:" . $e->getMessage();
+        }
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "Imposible conectar a la BD. Error:" . $e->getMessage();
     }
 
     return $respuesta;
 }
 
 
-function cambiar_foto($id,$foto)
+function cambiar_foto($id, $foto)
 {
-    try
-    {
-        $conexion=new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")); 
-        try
-        {
-            $consulta="update usuarios set foto=? where id_usuario=?";
-            
-            $sentencia=$conexion->prepare($consulta);
-            $sentencia->execute([$foto,$id]);
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        try {
+            $consulta = "update usuarios set foto=? where id_usuario=?";
 
-            $respuesta["mensaje"]="Foto actualizada";
-            
+            $sentencia = $conexion->prepare($consulta);
+            $sentencia->execute([$foto, $id]);
 
-            $sentencia=null;
-            $conexion=null;
+            $respuesta["mensaje"] = "Foto actualizada";
+
+
+            $sentencia = null;
+            $conexion = null;
+        } catch (PDOException $e) {
+            $respuesta["mensaje_error"] = "Imposible realizar la consulta. Error:" . $e->getMessage();
         }
-        catch(PDOException $e)
-        {
-            $respuesta["mensaje_error"]="Imposible realizar la consulta. Error:".$e->getMessage();
-        }
-        
-
-    }
-    catch(PDOException $e)
-    {
-        $respuesta["mensaje_error"]="Imposible conectar a la BD. Error:".$e->getMessage();
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "Imposible conectar a la BD. Error:" . $e->getMessage();
     }
 
     return $respuesta;
 }
-?>
